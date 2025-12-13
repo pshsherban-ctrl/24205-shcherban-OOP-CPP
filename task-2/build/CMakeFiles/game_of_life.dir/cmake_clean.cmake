28b43195908722a074b0d4202d59da447509b469
Pavel <p.shsherban@g.nsu.ru>
2025-12-13 17:01:31 +0700

    1

---
+++ task-2/build/CMakeFiles/game_of_life.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_of_life.dir/main.cpp.o"
+  "CMakeFiles/game_of_life.dir/main.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/command_line_parser.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/command_line_parser.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/game_controller.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/game_controller.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/preset_universes.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/preset_universes.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/rule.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/rule.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/universe.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/universe.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/universe_file_handler.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/universe_file_handler.cpp.o.d"
+  "CMakeFiles/game_of_life.dir/src/universe_renderer.cpp.o"
+  "CMakeFiles/game_of_life.dir/src/universe_renderer.cpp.o.d"
+  "game_of_life"
+  "game_of_life.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_of_life.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
